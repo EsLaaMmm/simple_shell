91c84f57aaa36d102b234d47bcbf1d491f7b7160
Olajide <larrm_ola@live.com>
2023-05-11 18:53:55 -0700

    HEADER File

---
+++ shell.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_SIZE 1024
+#define MAX_TOKEN_SIZE 64
+#define MAX_NUM_TOKENS 64
+
+char* tokens[MAX_NUM_TOKENS];
+int numTokens = 0;
