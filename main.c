2d4fd1ebad6b6d38ea8186f8f5389b08a040c493
Olajide <larrm_ola@live.com>
2023-05-12 18:12:09 -0700

    MAIN File

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include "shell.h"
 
-char *_getenv(const char *name);
-
 /**
  * main - Unix Command Line Interpreter
  * Return: 0
@@ -9,112 +7,44 @@ char *_getenv(const char *name);
 int main(void)
 {
 	pid_t pid;
-	char *args[MAX_ARGS], *token, *command = NULL, *path = NULL;
-	size_t bufsize = 0;
-	int status, i = 0;
-	char *command_path = malloc(strlen(token) + strlen(args[0]) + 2);
-
-	path = _getenv("PATH"):
+	char *args[] = {command, NULL};
 
 	while (1)
 	{
 		/* Display prompt and wait for user input */
 		printf("#cisfun$ ");
 		fflush(stdout);
-		/* Read user input */
-		if (getline(&command, &bufsize, stdin) == -1)
+		if (fgets(command, MAX_COMMAND_SIZE, stdin) == NULL)
 		{
 			/* Handle end of file (Ctrl+D) */
 			printf("\n");
-			exit(EXIT_SUCCESS);
+			exit(0);
 		}
 		/* Remove trailing newline character */
 		command[strcspn(command, "\n")] = '\0';
-		/* Split input into tokens */
-		token = strtok(command, " ");
-		while (token != NULL && i < MAX_ARGS - 1)
-		{
-			args[i++] = token;
-			token = strtok(NULL, " ");
-		}
-		args[i] = NULL;
-		/* Checks for built-in command */
-		if (strcmp(args[0], "exit") == 0)
-		{
-			exit(EXIT_SUCCESS);
-		}
-		else if (strcmp(args[0], "cd") == 0)
-		{
-			if (args[1] == NULL)
-			{
-				/* Change to home directory */
-				chdir(_getenv("HOME"));
-			}
-			else if (chdir(args[1]) == -1)
-			{
-				perror("cd");
-			}
-			continue;
-		}
-		/* Search for command in PATH */
-		path = _getenv("PATH");
-		token = strtok(path, ":");
-		while (token != NULL)
-		{
-			sprintf(command_path, "%s/%s", token, args[0]);
-			if (access(command_path, X_OK) == 0)
-			{
-				args[0] = command_path;
-				break;
-			}
-			free(command_path);
-			token = strtok(NULL, ":");
-		}
 		/* Fork a child process to execute command */
 		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
+		if (pid == 0)
 		{
 			/* Child process */
-			if (execve(args[0], args, NULL) == -1)
+			if (execve(command, args, environ) == -1)
 			{
-				perror("execve");
-				exit(EXIT_FAILURE);
+				printf("%s: command not found\n", command);
+				exit(1);
 			}
+		}
+		else if (pid < 0)
+		{
+			/* Handle fork error */
+			printf("Fork failed\n");
+			exit(1);
 		}
 		else
 		{
 			/* Parent process */
-			waitpid(pid, &status, 0);
+			waitpid(pid, NULL, 0);
+			printf("\n");
 		}
 	}
-	free(command);
 	return (0);
-}
-
-/**
- * _getenv - Gets the value of an enviroment variable
- * @name: Name of the variable
- * Return: Value of the variable or NULL if not found
- */
-char *_getenv(const char *name)
-{
-	int i;
-	size_t len = strlen(name);
-	char *env, *result = NULL;
-
-	for (i = 0; environ[i] != NULL; i++)
-	{
-		env = (environ[i]);
-		if (strcmp(env, name) == 0 && env[len] == '=')
-		{
-			return (&env[len + 1]);
-			break;
-		}
-	}
-	return (result);
 }
--- shell.h
+++ shell.h
@@ -3,19 +3,9 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <dirent.h>
-#include <errno.h>
 
-#define MAX_COMMAND_SIZE 1024
-#define MAX_ARGS 64
-
-extern char environ;
+#define MAX_COMMAND_SIZE 64
 
 /* MAIN FILE */
-void execute_command(char **args);
-char **parse_command(char *command);
-int check_builtin(char **args);
-void print_error(char *command, int code);
-void free_args(char **args);
+char command[MAX_COMMAND_SIZE];
+extern char **environ;
