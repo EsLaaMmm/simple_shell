2002f782e2ff97ea510a4b54cdb611ee78229b16
Olajide <larrm_ola@live.com>
2023-05-15 13:44:07 -0700

    STRINGS

---
+++ strings.c
@@ -0,0 +1,48 @@
+#include "shell.h"
+
+/**
+ * strlen - Computes the length of a string
+ * @s: String
+ * Return: Length of the string
+ */
+size_t strlen(const char *s)
+{
+	size_t i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * strcpy - Copies a string
+ * @dest: Destination string
+ * @src: Source string
+ * Return: Pointer to destination string
+ */
+char *strcpy(char *dest, const char *src)
+{
+	int i;
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Destination string
+ * @src: Source string
+ * Return: Pointer to destination string
+ */
+char *_strcat(char *dest, const char *src)
+{
+	int dest_len = strlen(dest);
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+
+	dest[dest_len + i] = '\0';
+
+	return dest;
+}
