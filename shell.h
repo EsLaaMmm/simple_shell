c20b5ba4c24d144a2d138e8adfef70d9b700bfee
Olajide <larrm_ola@live.com>
2023-05-14 17:12:38 -0700

    HEADER File

--- shell.h
+++ shell.h
@@ -1,15 +1,22 @@
+#ifndef SHELL_H
+#define SHELL_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 #define MAX_COMMAND_SIZE 1024
 #define MAX_ARGUMENTS 128
 
 /* MAIN FILE */
 int execute(char **args, char **envp);
+char **tokenize(char *line);
 
 /* PATH */
+char **get_paths(char **envp);
 char *check_path(char *command, char **envp);
 
+#endif
