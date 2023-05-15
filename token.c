9b92a00206ecbf5074ba2ba3055d2a0ca18dd740
Olajide <larrm_ola@live.com>
2023-05-14 17:39:16 -0700

    Tokenize a string

---
+++ token.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+
+/**
+ * tokenize - breaks a string into tokens
+ * @str: string to tokenize
+ *
+ * Return: array of tokens
+ */
+char **tokenize(char *str)
+{
+        char **tokens;
+        char *token;
+        int i, n = 0;
+
+        /* count number of tokens */
+        for (i = 0; str[i]; i++)
+        {
+                if (str[i] == ' ')
+                        continue;
+                n++;
+                while (str[i] && str[i] != ' ')
+                        i++;
+        }
+
+        /* allocate memory for token array */
+        tokens = malloc(sizeof(char *) * (n + 1));
+        if (!tokens)
+        {
+                perror("malloc");
+                exit(EXIT_FAILURE);
+        }
+
+        /* tokenize string */
+        token = strtok(str, " \n\t");
+        for (i = 0; token; i++)
+        {
+                tokens[i] = token;
+                token = strtok(NULL, " \n\t");
+        }
+        tokens[i] = NULL;
+
+        return (tokens);
+}
