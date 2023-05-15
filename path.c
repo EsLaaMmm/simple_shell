828695dedfffd2e13c504f1a1196f524c580997d
Olajide <larrm_ola@live.com>
2023-05-14 19:04:06 -0700

    Handles the PATH

--- path.c
+++ path.c
@@ -1,6 +1,54 @@
 #include "shell.h"
 
 /**
+ * get_path - Returns an array of directories in the PATH
+ * @envp: Environment variables
+ * Return: Array of directories in the PATH environment variable
+ */
+char **get_path(char **envp)
+{
+        char **path = NULL;
+        char *path_env;
+	char *c;
+        char *dir;
+        int i, n;
+
+        /* find PATH environment variable */
+        for (i = 0; envp[i]; i++)
+        {
+                if (strncmp(envp[i], "PATH=", 5) == 0)
+                {
+                        /* allocate memory for path array */
+                        path_env = envp[i] + 5;
+                        n = 1; /* count number of directories in path */
+                        for (c = path_env; *c != '\0'; c++)
+                        {
+                                if (*c == ':')
+                                        n++;
+                        }
+                        path = malloc(sizeof(char *) * (n + 1));
+                        if (!path)
+                        {
+                                perror("malloc");
+                                exit(EXIT_FAILURE);
+                        }
+
+                        /* tokenize path_env string */
+                        dir = strtok(path_env, ":");
+                        for (i = 0; dir; i++)
+                        {
+                                path[i] = dir;
+                                dir = strtok(NULL, ":");
+                        }
+                        path[i] = NULL;
+                        break;
+                }
+        }
+
+        return (path);
+}
+
+/**
  * check_path - Checks if command exists in any of the PATH directories
  * @command: Command to check
  * @envp: Environment variables
@@ -14,7 +62,7 @@ char *check_path(char *command, char **envp)
 	int i;
 
 	/* get array of directories in PATH */
-	paths = get_paths(envp);
+	paths = get_path(envp);
 
 	/* check if command exists in each directory */
 	for (i = 0; paths && paths[i]; i++)
