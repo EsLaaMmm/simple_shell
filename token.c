f837cfceca318e451b2c1602b411851d1778eb49
Olajide <larrm_ola@live.com>
2023-05-15 14:35:37 -0700

    Tokenize a string

--- token.c
+++ token.c
@@ -7,28 +7,39 @@
  */
 char **tokenize(char *str)
 {
-        char **tokens = NULL;
-        char *token;
-        int i = 0;
+	char **tokens = NULL;
+	char *token, **new_tokens;
+	int i = 0;
+	const int token_size = 64;
+	int buffer_size = token_size;
 
 	/* allocate memory for array */
 	tokens = malloc(sizeof(char *));
 	if (!tokens)
 		return (NULL);
 	/* tokenize string */
-        token = strtok(str, " \t\n");
-        while (token)
-        {
+	token = strtok(str, " \t\n");
+	tokens[0] = NULL;
+	while (token)
+	{
 		/* add token to array */
-                tokens[i] = token;
+		tokens[i] = token;
 		i++;
-                tokens = realloc(tokens, sizeof(char *) * (i + 1));
-		if (!tokens)
-			return (NULL);
+		if (i >= buffer_size)
+		{
+			buffer_size += token_size;
+			new_tokens = realloc(tokens, buffer_size * sizeof(char *) * (i + 2));
+			if (!new_tokens)
+			{
+				free(tokens);
+				return (NULL);
+			}
+			tokens = new_tokens;
+		}
 		token = strtok(NULL, " \t\n");
-        }
+	}
 	/* add NULL terminator to end of array */
-        tokens[i] = NULL;
+	tokens[i] = NULL;
 
-        return (tokens);
+	return (tokens);
 }
