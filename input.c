ac3f9e2127a629836127488a99e12f08a6406dbf
Olajide <larrm_ola@live.com>
2023-05-22 04:31:47 -0700

    Reads user input

--- input.c
+++ input.c
@@ -20,7 +20,7 @@ int input(char *file_name)
 
 	while (_getline(&line, &buffer_size, fp) != -1)
 	{
-		char **args = parse_line(line);
+		char **args = parse_input(line);
 
 		if (args[0] != NULL)
 		{
@@ -83,4 +83,43 @@ char **parse_input(char *line)
 	}
 	tokens[pos] = NULL;
 	return (tokens);
+}
+
+/**
+ * script - Runs script from given file
+ * @file_name: Name of the file
+ * Return: 0 on sucess, 1 on failure
+ */
+int script(char *file_name)
+{
+	FILE *fp = fopen(file_name, "r");
+	char *line = NULL;
+	size_t buffer_size = 0;
+
+	if (fp == NULL)
+	{
+		perror("script");
+		return (1);
+	}
+
+	while (_getline(&line, &buffer_size, fp) != -1)
+	{
+		char **args = parse_input(line);
+
+		if (args[0] != NULL)
+		{
+			if (_strcmp(args[0], "exit") == 0)
+			{
+				exit_shell(args);
+			}
+			else
+			{
+				execute_command(args);
+			}
+		}
+		free(args);
+	}
+	free(line);
+	fclose(fp);
+	return (0);
 }
