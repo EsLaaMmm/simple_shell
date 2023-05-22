269ca0d62ad9c6410bb3366a1d8d0463d7d8d302
Olajide <larrm_ola@live.com>
2023-05-22 05:39:34 -0700

    HEADER File

--- shell.h
+++ shell.h
@@ -6,21 +6,49 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
+#include <limits.h>
+#include <errno.h>
 
-#define MAX_COMMAND_SIZE 1024
-#define MAX_ARGUMENTS 128
+#define BUFFER_SIZE 1024
+#define TOKEN_DELIM " \t\r\n\a"
 
-/* MAIN FILE */
-int execute(char **args, char **envp);
+/* MAIN */
+void prompt(void);
+int main(int argc, char **argv);
 
-/* TOKENIZE */
-char **tokenize(char *line);
+/* CHANGE DIRECTORY */
+int change_directory(char **args);
 
-/* PATH */
-char **get_path(char **envp);
-char *check_path(char *command, char **envp);
+/* ECHO COMMAND */
+int echo(char **args);
 
-#endif
+/* ENVIROMENT */
+int set_environment(char **args);
+int unset_environment(char **args);
+
+/* EXECUTE COMMAND */
+int execute_command(char **args);
+int launch(char **args);
+
+/* EXIT SHELL */
+int exit_shell(char **args);
+
+/* HANDLE ERROR/ALIAS */
+void handle_error(char *message);
+int handle_alias(char **args);
+
+/* COMMAND INPUT */
+int input(char *file_name);
+char **parse_input(char *line);
+int script(char *file_name);
+
+/* COMMAND LINE */
+ssize_t _getline(char **ptr_line, size_t *n, FILE *ptr_stream);
+char *read_line(void);
+
+/* STRINGS */
+int _strcmp(const char *str1, const char *str2);
+size_t _strlen(const char *str);
+
+#endif /* SHELL_H */
+
